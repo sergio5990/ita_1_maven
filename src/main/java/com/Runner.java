518e47953ad925761c536ff0a4d5f4c086a6158c
sergey-kruk <sergey-kruk>
2019-07-06 21:29:48 +0300

    cobertura

--- src/main/java/com/Runner.java
+++ src/main/java/com/Runner.java
@@ -2,6 +2,9 @@ package com;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Runner {
     public static void main(String[] args) {
         printName("Sergey");
@@ -9,5 +12,9 @@ public class Runner {
 
     static void printName(String name) {
         System.out.println("Name : " + StringUtils.upperCase(name));
+    }
+
+    static List<String> splitTest(String test){
+        return Arrays.asList(test.split(","));
     }
 }
--- src/test/java/com/RunnerTest.java
+++ src/test/java/com/RunnerTest.java
@@ -1,12 +1,31 @@
 package com;
 
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RunnerTest {
 
     @Test
     public void printName() {
         Runner.printName("ab");
+    }
+
+
+    @Test
+    @DisplayName("split without whitespaces")
+    void split3() {
+        List<String> strings = Runner.splitTest("a,b,c");
+        assertEquals(strings.get(0), "a");
+        assertEquals(strings.get(1), "b");
+        assertEquals(strings.get(2), "c");
+        assertAll("all",
+                () -> assertEquals(strings.get(1), "b"),
+                () -> assertEquals(strings.get(2), "c"));
     }
 }
