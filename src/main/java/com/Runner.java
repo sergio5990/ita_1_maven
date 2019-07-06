5d92ffe22ade64cd8efb0c9af7aebc501990842b
sergey-kruk <sergey-kruk>
2019-07-06 20:10:03 +0300

    /maven_1/src/main/java
    javac -cp /Users/skruk/Documents/work/it_academy/code/maven_1/lib/commons-lang3-3.9.jar com/Runner.java
    
    java -cp .:/Users/skruk/Documents/work/it_academy/code/maven_1/lib/commons-lang3-3.9.jar com.Runner

--- src/main/java/com/Runner.java
+++ src/main/java/com/Runner.java
@@ -1,11 +1,13 @@
 package com;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Runner {
     public static void main(String[] args) {
         printName("Sergey");
     }
 
     private static void printName(String name) {
-        System.out.println("Name : "+ name);
+        System.out.println("Name : " + StringUtils.upperCase(name));
     }
 }
