04e1791c6cefb4b95278c7bae4d05c7ddd73abee
sergey-kruk <sergey-kruk>
2019-07-06 20:05:44 +0300

    /maven_1/src/main/java javac com/Runner.java java com.Runner

---
+++ src/main/java/com/Runner.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Runner {
+    public static void main(String[] args) {
+        printName("Sergey");
+    }
+
+    private static void printName(String name) {
+        System.out.println("Name : "+ name);
+    }
+}
