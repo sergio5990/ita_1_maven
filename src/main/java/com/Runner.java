4d62328c2ee9bb6b3560f74e3ca5a2a34cfe61b2
sergey-kruk <sergey-kruk>
2019-08-26 00:40:09 +0300

    fix performance

--- src/main/java/com/Runner.java
+++ src/main/java/com/Runner.java
@@ -5,11 +5,8 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class Runner {
-    private static final Pattern COMPILE = Pattern.compile(",");
-
     public static void main(String[] args) {
         printName("Sergey");
     }
@@ -22,6 +19,6 @@ public class Runner {
         if (test == null) {
             return Collections.emptyList();
         }
-        return Arrays.asList(COMPILE.split(test, -1));
+        return Arrays.asList(test.split(","));
     }
 }
