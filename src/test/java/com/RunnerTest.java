64945b16cf1360ad565a72d266b78415a43089a5
sergio5990 <sergey-kruk>
2020-03-02 20:56:28 +0300

    new test

--- src/test/java/com/RunnerTest.java
+++ src/test/java/com/RunnerTest.java
@@ -20,7 +20,10 @@ public class RunnerTest {
     @Test
     @DisplayName("split without whitespaces")
     void split3() {
-        List<String> strings = Runner.splitTest("a,b,c");
+        final String test = "a,b,c";
+
+        List<String> strings = Runner.splitTest(test);
+
         assertEquals(strings.get(0), "a");
         assertEquals(strings.get(1), "b");
         assertEquals(strings.get(2), "c");
@@ -32,8 +35,21 @@ public class RunnerTest {
     @Test
     @DisplayName("split null")
     void splitNull() {
+
         List<String> strings = Runner.splitTest(null);
+
         assertTrue(strings.isEmpty());
 
+    }
+
+    @Test
+    @DisplayName("split null")
+    void splitNN() {
+        final String test = "1,2,3";
+
+        List<String> strings = Runner.splitTest(test);
+
+        assertFalse(strings.isEmpty());
+        assertEquals(3, strings.size());
     }
 }
